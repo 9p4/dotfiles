7fab10ed823c66c92d8dd17d0fee8fc527b44406
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2021-11-30 22:44:30 -0500

    Update systemd units, fix vim format on save

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -10,7 +10,7 @@ an executable
 
 -- general
 lvim.log.level = "warn"
-lvim.format_on_save = true
+lvim.format_on_save = false
 lvim.colorscheme = "material"
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
