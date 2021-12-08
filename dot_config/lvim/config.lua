d0cc30f8ce848e545764abbe51e15bf2dcba1ba7
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2021-12-08 18:31:32 -0500

    Add norg to lvim and fix sway bind for polkit

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -150,6 +150,7 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- Additional Plugins
 lvim.plugins = {
   'marko-cerovac/material.nvim',
+  'nvim-lua/plenary.nvim',
   'vhyrro/neorg',
   "https://github.com/rhysd/vim-grammarous"
 
@@ -172,6 +173,45 @@ require('neorg').setup {
                 }
             }
         }
+    },
+    hook = function ()
+      -- This sets the leader for all Neorg keybinds. It is separate from the regular <Leader>,
+      -- And allows you to shove every Neorg keybind under one "umbrella".
+      local neorg_leader = "<Leader>o" -- You may also want to set this to <Leader>o for "organization"
+
+      -- Require the user callbacks module, which allows us to tap into the core of Neorg
+      local neorg_callbacks = require('neorg.callbacks')
+
+      -- Listen for the enable_keybinds event, which signals a "ready" state meaning we can bind keys.
+      -- This hook will be called several times, e.g. whenever the Neorg Mode changes or an event that
+      -- needs to reevaluate all the bound keys is invoked
+      neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
+
+        -- Map all the below keybinds only when the "norg" mode is active
+        keybinds.map_event_to_mode("norg", {
+          n = { -- Bind keys in normal mode
+
+            -- Keys for managing TODO items and setting their states
+            { "nd", "core.norg.qol.todo_items.todo.task_done"},
+            { "nu", "core.norg.qol.todo_items.todo.task_undone" },
+            { "np", "core.norg.qol.todo_items.todo.task_pending" },
+            { "<C-Space>", "core.norg.qol.todo_items.todo.task_cycle" }
+
+          },
+        }, { silent = true, noremap = true })
+
+      end)
+    end,
+    requires = "nvim-lua/plenary.nvim"
+}
+
+local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
+parser_configs.norg = {
+    install_info = {
+        url = "https://github.com/nvim-neorg/tree-sitter-norg",
+        files = { "src/parser.c", "src/scanner.cc" },
+        branch = "main"
     },
 }
 
